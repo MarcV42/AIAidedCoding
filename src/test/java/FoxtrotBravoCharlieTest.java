b1c56c2b5d7f8aefe543c43925b6e3ffdc30687f
MVe11111 <90624942+Sprudelflasch@users.noreply.github.com>
2023-09-06 12:02:28 +0200

    Mit Test

---
+++ src/test/java/FoxtrotBravoCharlieTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FoxtrotBravoCharlieTest {
+
+    @Test
+    public void testBubbleSort() {
+        int[] inputArray = {5, 2, 9, 1, 5, 6};
+        int[] expectedSortedArray = {1, 2, 5, 5, 6, 9};
+
+        FoxtrotBravoCharlie.golfHotel(inputArray);
+
+        assertArrayEquals(expectedSortedArray, inputArray);
+    }}
